9c914ee66e622d2931e78495826085d51ad3fd2f
krishna-chaithanya-jakka <krishna.jakka@techmojo.in>
2021-01-17 18:55:41 +0530

    added ployfill

---
+++ polyfill.ts
@@ -0,0 +1,8 @@
+if (!Element.prototype.matches) {
+    Element.prototype.matches = Element.prototype["msMatchesSelector"];
+}
+
+import "reflect-metadata";
+if (!Element.prototype.matches) {​​​​​​​​
+Element.prototype.matches = Element.prototype["msMatchesSelector"];
+ }​​​​​​​​
