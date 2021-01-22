104d929836703c45ec2e8925891289f245f4e6f1
krishna-chaithanya-jakka <krishna.jakka@techmojo.in>
2021-01-22 23:35:01 +0530

    deleted all the files

--- polyfill.ts
+++
@@ -1,8 +0,0 @@
-if (!Element.prototype.matches) {
-    Element.prototype.matches = Element.prototype["msMatchesSelector"];
-}
-
-import "reflect-metadata";
-if (!Element.prototype.matches) {​​​​​​​​
-Element.prototype.matches = Element.prototype["msMatchesSelector"];
- }​​​​​​​​
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './../polyfill.ts'
+
 
 ReactDOM.render(
   <React.StrictMode>
